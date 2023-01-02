6357d3a87c47bc1ce93ca3d77ad8a65762bc77ba
INatsukiI <e1q20019@oit.ac.jp>
2023-01-02 19:16:39 +0900

    プロフィール作成のバグを見つけたので修正

--- pages/create.tsx
+++ pages/create.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import UserForm from "../components/user-form";
 
 const CreateAccount = () => {
-  return <UserForm />;
+  return <UserForm isEditMode={false} />;
 };
 
 export default CreateAccount;
