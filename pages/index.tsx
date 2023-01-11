2488964d88d6be40c8794c14b0d45d975e69379f
INatsukiI <e1q20019@oit.ac.jp>
2023-01-11 15:00:42 +0900

    Topページ作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,29 @@ import { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => {
   return (
-    <div className="container">
-      <Link href="/login">認証ページへ</Link>
+    <div>
+      <div className="animate-tracking-in-expand-fwd-bottom">
+        <h2 className="mt-16 text-3xl">Health Recoder リリースノート</h2>
+        <div className="p-6">
+          <h2 className="text-2xl mb-4">Version 1.0</h2>
+          <p>2022年1月11日</p>
+          <ul className="list-inside list-disc">
+            <li>アプリをリリースしました!</li>
+            <li>ヘッダーのLoginリンクからログインしてお使いください.</li>
+          </ul>
+        </div>
+      </div>
+      <div className="mt-16 animate-tracking-in-expand-fwd-bottom">
+        <h2 className="mt-16 text-3xl">お問い合わせ</h2>
+        <div className="p-6">
+          <h2 className="text-2xl mb-4">Contact me</h2>
+          <p className="leading-relaxed">
+            お問い合わせは下記の連絡先にご連絡ください
+          </p>
+          <p className="leading-relaxed">Twitter: 準備中...</p>
+          <p className="leading-relaxed">Gmail: natukiomura@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 };
