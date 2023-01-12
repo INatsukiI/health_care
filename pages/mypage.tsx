efc66fa67df47983f7188e862a9cb4d95aed9165
INatsukiI <e1q20019@oit.ac.jp>
2023-01-12 14:26:07 +0900

    マイページレイアウト調整

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import Layout from "../components/layouts/layout";
@@ -15,15 +16,29 @@ const Mypage: NextPageWithLayout = () => {
   if (!fbUser) {
     router.push("/");
     return null;
-  } else {
-    console.log(fbUser);
   }
 
   return (
-    <div>
-      <h1>マイページ</h1>
-      <p>名前:{user && user.name}</p>
-      <p>プロフィール:{user && user.profile}</p>
+    <div className="flex flex-col items-center">
+      <div className="mt-16 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          アカウント情報
+        </h5>
+        <div className="mx-auto">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            名前:{user?.name}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            プロフィール:{user?.profile}
+          </p>
+          <Link
+            href="/edit_profile"
+            className="px-3 py-2 text-sm font-medium text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            編集
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
