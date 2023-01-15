a3a290aca089dd7ccfe9194b9e160bb04a981eba
INatsukiI <e1q20019@oit.ac.jp>
2023-01-15 18:36:00 +0900

    h1タグ削除

--- components/user-form.tsx
+++ components/user-form.tsx
@@ -53,7 +53,6 @@ const UserForm = ({ isEditMode }: { isEditMode: boolean }) => {
 
   return (
     <div>
-      <h1>{isEditMode ? "プロフィール編集" : "アカウント作成"}</h1>
       <div className="mt-16 flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
